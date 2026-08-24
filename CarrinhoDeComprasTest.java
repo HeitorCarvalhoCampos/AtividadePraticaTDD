318716bef2f20058d9394beaa093381b8094877c
HeitorCarvalhoCampos <hcarvalhocampos2008@gmail.com>
2026-08-23 23:59:14 -0300

    Novos testes com retorno de calculo liquido total e bruto

--- CarrinhoDeComprasTest.java
+++ CarrinhoDeComprasTest.java
@@ -65,30 +65,64 @@ public class CarrinhoDeComprasTest {
         });
     }
 
+    // @Test
+    // public void testDeveCalcularValorDaCompra() {
+    // CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+    // Item item1 = new Item("Produto A", 10.00, 2);
+    // Item item2 = new Item("Produto B", 15.00, 1);
+
+    // carrinho.adicionarItem(item1);
+    // carrinho.adicionarItem(item2);
+
+    // double valorTotal = carrinho.calcularValorTotal();
+
+    // assertEquals(35.00, valorTotal, 0.01);
+    // }
+
+    // @Test
+    // public void
+    // testDeveCalcularValorDaCompraComDescontoQuandoValorBrutoForMaiorQue500() {
+    // CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+    // Item item1 = new Item("Produto A", 100.00, 6);
+
+    // carrinho.adicionarItem(item1);
+
+    // double valorTotal = carrinho.calcularValorTotal();
+
+    // assertEquals(540.00, valorTotal, 0.01);
+    // }
+
+    // Testes para novo formato de função calcularValorTotal() que retorna uma lista
+    // com valor bruto e liquido
     @Test
     public void testDeveCalcularValorDaCompra() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         Item item1 = new Item("Produto A", 10.00, 2);
-        Item item2 = new Item("Produto B", 15.00, 1);
+        Item item2 = new Item("Produto B", 5.00, 5);
 
         carrinho.adicionarItem(item1);
         carrinho.adicionarItem(item2);
 
-        double valorTotal = carrinho.calcularValorTotal();
+        double valorEsperado = 45;
 
-        assertEquals(35.00, valorTotal, 0.01);
+        assertEquals(valorEsperado, carrinho.calcularValorTotal().get(0), 0.01);
+        assertEquals(valorEsperado, carrinho.calcularValorTotal().get(1), 0.01);
     }
 
     @Test
     public void testDeveCalcularValorDaCompraComDescontoQuandoValorBrutoForMaiorQue500() {
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         Item item1 = new Item("Produto A", 100.00, 6);
+        Item item2 = new Item("Produto B", 100.00, 6);
 
         carrinho.adicionarItem(item1);
+        carrinho.adicionarItem(item2);
 
-        double valorTotal = carrinho.calcularValorTotal();
+        double valorBruto = 1200;
+        double valorLiquido = 1080; // 10% de desconto
 
-        assertEquals(540.00, valorTotal, 0.01);
+        assertEquals(valorBruto, carrinho.calcularValorTotal().get(0), 0.01);
+        assertEquals(valorLiquido, carrinho.calcularValorTotal().get(1), 0.01);
     }
 
 }
