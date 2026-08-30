fba9a570360b17642c8a80ede2ae1715ff28b0f5
HeitorCarvalhoCampos <hcarvalhocampos2008@gmail.com>
2026-08-30 00:12:13 -0300

    Ajuste nos arquivos e configurações para execução de testes

--- src/test/java/CarrinhoDeComprasTest.java
+++ src/test/java/CarrinhoDeComprasTest.java
@@ -1,5 +1,10 @@
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CarrinhoDeComprasTest {
 
     @Test
